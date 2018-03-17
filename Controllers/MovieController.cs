ec9ed733f78500c5f1c7633a82eb3215bc0d4815
Claire Krueger <clairekrueger21@Claires-Air.attlocal.net>
2018-03-17 16:07:54 -0500

    Full CRUD

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -30,5 +30,48 @@ namespace media_library_api.Controllers {
             }
             return new ObjectResult (item);
         }
+
+        [HttpPost]
+        public IActionResult Create ([FromBody] MovieItem item) {
+            if (item == null) {
+                return BadRequest ();
+            }
+
+            _context.MovieItems.Add (item);
+            _context.SaveChanges ();
+
+            return CreatedAtRoute ("GetMovie", new { id = item.Id }, item);
+        }
+
+        [HttpPut ("{id}")]
+        public IActionResult Update (long id, [FromBody] MovieItem item) {
+            if (item == null || item.Id != id) {
+                return BadRequest ();
+            }
+
+            var todo = _context.MovieItems.FirstOrDefault (t => t.Id == id);
+            if (todo == null) {
+                return NotFound ();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.MovieItems.Update (todo);
+            _context.SaveChanges ();
+            return new NoContentResult ();
+        }
+
+        [HttpDelete ("{id}")]
+        public IActionResult Delete (long id) {
+            var todo = _context.MovieItems.FirstOrDefault (t => t.Id == id);
+            if (todo == null) {
+                return NotFound ();
+            }
+
+            _context.MovieItems.Remove (todo);
+            _context.SaveChanges ();
+            return new NoContentResult ();
+        }
     }
 }
